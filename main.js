9c079deb92600248464427ae5e1a4e336b89edad
max <maxirochow27@gmail.com>
2025-01-05 22:49:25 -0300

    second commit

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ const listaPokemons = document.querySelector('#listaPokemon');
 const botonesHeader = document.querySelectorAll('.btn-header');
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 153; i++) {
+for (let i = 1; i <= 151; i++) {
     fetch(URL + i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data));
 }
 
 function mostrarPokemon(poke) {
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`); // Espacio antes de "tipo" corregido
-    tipos = tipos.join(' ');
+    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`); 
 
     let pokeId = poke.id.toString();
     if (pokeId.length === 1) {
@@ -46,7 +45,7 @@ function mostrarPokemon(poke) {
 }
 
 botonesHeader.forEach(boton => boton.addEventListener('click', (event) => {
-    const botonId = event.currentTarget.id; // Corregido de "event-currentTarger" a "event.currentTarget"
+    const botonId = event.currentTarger.id; // Corregido de "event-currentTarger" a "event.currentTarget"
 
     listaPokemons.innerHTML = '';
 
@@ -58,7 +57,7 @@ botonesHeader.forEach(boton => boton.addEventListener('click', (event) => {
                     mostrarPokemon(data);
                 } else {
                     const tipos = data.types.map(type => type.type.name);
-                    if (tipos.some(tipo => tipo.includes(botonId))) { // Corregido "tipos.some(tipos" a "tipos.some(tipo"
+                    if (tipos.some(tipos => tipos.includes(botonId))) { // Corregido "tipos.some(tipos" a "tipos.some(tipo"
                         mostrarPokemon(data);
                     }
                 }
