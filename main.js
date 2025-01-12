67a1641a88d5009df38cc67763bdb06102b7901b
masirochow19 <167722111+masirochow19@users.noreply.github.com>
2025-01-12 19:32:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function mostrarPokemon(poke) {
 
     let pokeId = poke.id.toString();
     if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
+        pokeId = "0" + pokeId;
     } else if (pokeId.length === 2) {
         pokeId = "0" + pokeId;
     }
